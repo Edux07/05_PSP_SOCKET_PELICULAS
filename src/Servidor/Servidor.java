c28a11c26425c3ea8b06a20ee70c740286cbe36b
Edu <118288657+Edux07@users.noreply.github.com>
2023-12-01 00:35:50 +0100

    ..

--- src/Servidor/Servidor.java
+++ src/Servidor/Servidor.java
@@ -1,7 +1,5 @@
 package Servidor;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -9,19 +7,19 @@ import java.util.List;
 
 public class Servidor {
 	public static final int PUERTO = 2180;
-	
-	protected List<Peliculas> pelis= new ArrayList<>();
+	protected static List<Peliculas> pelis = new ArrayList<>();
+
 	public static void main(String[] args) {
+		Biblioteca();
 		System.out.println("-------- Servidor  --------");
-	
 
 		try (ServerSocket server = new ServerSocket(PUERTO)) {
 			System.out.println("Esperando conexiones en el puerto " + PUERTO);
-			
+
 			while (true) {
 				Socket socketAlCliente = server.accept();
 				System.out.println("Nuevo cliente conectado.");
-				
+
 				new peliHilo(socketAlCliente).start();
 			}
 
@@ -29,5 +27,13 @@ public class Servidor {
 			System.err.println("Error en el servidor -> " + e);
 			e.printStackTrace();
 		}
+	}
+
+	protected static void Biblioteca() {
+		pelis.add(new Peliculas(1, "Star Wars III- La venganza de los Sith", "George Lucas"));
+		pelis.add(new Peliculas(2, "Kill Bill", "Quentin Tarantino"));
+		pelis.add(new Peliculas(3, "Star Wars VI- El retorno del Jedi ", "George Lucas"));
+		pelis.add(new Peliculas(4, "Malditos Bastardos", "Quentin Tarantino"));
+		pelis.add(new Peliculas(5, "Interestellar", "Christopher Nolan"));
 	}
 }
