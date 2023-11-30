bb1bddb0479d5917c17bd3439b6315353b98dbfe
Edu <118288657+Edux07@users.noreply.github.com>
2023-11-30 23:44:27 +0100

    .

--- src/Servidor/peliHilo.java
+++ src/Servidor/peliHilo.java
@@ -4,7 +4,7 @@ import java.net.Socket;
 
 public class peliHilo extends Thread {
 
-	private Socket socketAlCliente;
+	public Socket socketAlCliente;
 
 	public peliHilo(Socket socketAlCliente) {
 		this.socketAlCliente = socketAlCliente;
