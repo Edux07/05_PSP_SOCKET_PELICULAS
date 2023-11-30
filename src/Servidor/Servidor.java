22eb38d142aa0550340e0d603c86d62cfdd15055
Edu <118288657+Edux07@users.noreply.github.com>
2023-11-30 20:35:19 +0100

    .

--- src/Servidor/Servidor.java
+++ src/Servidor/Servidor.java
@@ -5,26 +5,24 @@ import java.net.Socket;
 
 public class Servidor {
 	public static final int PUERTO = 2180;
-public static void main(String[] args) {
-	System.out.println("-------- Servidor Juego de Piedra-Papel-Tijera --------");
 
-	try (ServerSocket server = new ServerSocket(PUERTO)) {
-		System.out.println("Esperando conexiones en el puerto " + PUERTO);
+	public static void main(String[] args) {
+		System.out.println("-------- Servidor  --------");
+
+		try (ServerSocket server = new ServerSocket(PUERTO)) {
+			System.out.println("Esperando conexiones en el puerto " + PUERTO);
 
 			while (true) {
 				Socket socketAlCliente = server.accept();
 				System.out.println("Nuevo cliente conectado.");
-				
-					if(server.accept() != null) {
-						new peliHilo(socketAlCliente).start();
-					}
-				}
-		
-	 }catch (Exception e) {
-		System.err.println("Error en el servidor -> " + e);
-		e.printStackTrace();
+
+				new peliHilo(socketAlCliente).start();
+
+			}
+
+		} catch (Exception e) {
+			System.err.println("Error en el servidor -> " + e);
+			e.printStackTrace();
+		}
 	}
 }
-}
-
-	
